9531520fae0b08a5c4050d91344311c37708214d
Dinesh <dineshsingh9350@gmail.com>
2023-08-10 13:52:31 +0530

    App.js

--- Chapter 01 Inception/Code/App.js
+++ Chapter 01 Inception/Code/App.js
@@ -1,4 +1,13 @@
-const heading1 = React.createElement('h1', { className: 'title1' }, 'Heading 1')
+const heading1 = React.createElement(
+  'h1',
+  {
+    style: {
+      backgroundColor: 'black',
+			color: "white"
+    },
+  },
+  'Heading 1'
+)
 const heading2 = React.createElement('h2', { className: 'title2' }, 'Heading 2')
 const container = React.createElement('div', { id: 'container' }, [
   heading1,
